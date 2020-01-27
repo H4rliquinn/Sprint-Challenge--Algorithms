d5a876b1eea0417d08b2afb86d06035cfd136290
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-27 01:29:58 -0500

    Working on rotating a matirx

---
+++ matrixrotate.py
@@ -0,0 +1,35 @@
+"""Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 to 9, write a 
+function rotate_image that receives a matrix as input and rotates the image by 90 degrees in 
+the counter-clockwise direction."""
+
+"""
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+"""
+line = range(10)
+matrix = [[*line] for x in range(10)]
+# matrix[4][5] = 5
+# temp = []
+
+
+def rotate_image(matrix):
+    for x in range(len(matrix)//2):
+        for y in range(len(matrix[x])-1-x):
+            # temp=(matrix[len(matrix)-(x+1)][x])
+            matrix[x][y+x] = '*'+str(matrix[x][x+y])
+    return matrix
+
+
+# for x in matrix:
+#     print(x)
+
+after = rotate_image(matrix)
+for x in after:
+    print(x)
